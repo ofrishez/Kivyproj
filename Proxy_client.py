b4b13ebc4fad22f17f84d599ebbce0e8a78730c3
ofrishez <60386539+ofrishez@users.noreply.github.com>
2020-01-30 12:32:16 +0200

    Add files via upload

--- Proxy_client.py
+++ Proxy_client.py
@@ -2,12 +2,31 @@ from classes import Serverlib
 import threading
 import time
 import atexit
+import sys
 
 
 def main():
-    proxy_server = Serverlib.Server(logging=True, malware_block=True, ad_filtering=True)
+    ads = input("ads?[y/n]\n")
+    if ads == "y":
+        ads = True
+    else:
+        ads = False
+    malware = input("malwares?[y/n]\n")
+    if malware == "y":
+        malware = True
+    else:
+        malware = False
+    proxy_server = Serverlib.Server(logging=True, malware_block=malware, ad_filtering=ads)
     threading.Thread(target=proxy_server.start_server).start()
     proxy_server.config_proxy_windows()
+    while 1:
+        x= input()
+        if x=="close":
+            proxy_server.shutdown()
+        if x == "quit":
+            proxy_server.shutdown()
+            sys.exit(1)
+
 #    atexit.register(proxy_server.shutdown)
 #    time.sleep(15)
 #    proxy_server.shutdown()
